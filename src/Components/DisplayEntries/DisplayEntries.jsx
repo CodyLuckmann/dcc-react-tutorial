3a281308d6bdde679480dfb99ab2f75c02824d2a
CodyLuckmann <cluckmann@live.com>
2022-01-05 11:21:15 -0600

    added chart tracker and changed display entries key to index

--- src/Components/DisplayEntries/DisplayEntries.jsx
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -1,24 +1,24 @@
 
 const DisplayEntries = (props) => {
     return ( 
-    <table>
-        <thead>
-          <tr>
-            <th>Entry Number</th>
-            <th>Weight</th>
-            <th>Date</th>
+       <table>
+         <thead>
+         <tr>
+              <th>Entry Number</th>
+              <th>Weight</th>
+              <th>Date</th>
           </tr>
         </thead>
         <tbody>
-         {props.parentEntries.map((entry, index) => {
-           return(
-             <tr>
-               <td>{index+1}</td>
-               <td>{entry.weight}</td>
-               <td>{entry.date}</td>
-             </tr>
-           );
-         })}
+        {props.parentEntries.map((entry, index) => {
+          return(
+          <tr key ={index}>
+              <td>{index+1}</td>
+              <td>{entry.weight}</td>
+              <td>{entry.date}</td>
+          </tr>
+          );
+        })}
         </tbody>
     </table>
      );
